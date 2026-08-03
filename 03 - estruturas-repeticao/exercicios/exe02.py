e7adef1309ca6ee56ac49966b955c8aee289aab2
eduardoliria-senai <eduardo.liria@docente.senai.br>
2026-08-03 20:32:30 -0300

    Conteudo While

--- 03 - estruturas-repeticao/exercicios/exe02.py
+++ 03 - estruturas-repeticao/exercicios/exe02.py
@@ -1,23 +1,16 @@
 # Pergunte ao usuario 5 numeros e diga se o numero e positivo ou negativo
 # Exemplo: 1 positivo, -2 negativo...  
 
-# for i in range(5):
-#     numero = int(input('Digite um numero: '))
+for i in range(5):
+    numero = int(input('Digite um numero: '))
 
-#     if numero > 0:
-#         print(f'O numero {numero} que voce digitou é positivo')
-#     elif numero < 0:
-#         print(f'O numero {numero} que voce digitou é negativo')
-#     else:
-#         print(f'O numero é igual a 0')
+    if numero > 0:
+        print(f'O numero {numero} que voce digitou é positivo')
+    elif numero < 0:
+        print(f'O numero {numero} que voce digitou é negativo')
+    else:
+        print(f'O numero é igual a 0')
 
 
-
-lista = [20,30,40,50,65.8,23,81]
-# soma = 0
-
-for i in range(len(lista)):
-    if lista[i] > 37:
-        print(lista[i])
 
 
