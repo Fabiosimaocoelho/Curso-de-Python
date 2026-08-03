42dffc410de93921549c4347066910911f34bac0
Fabiosimaocoelho <coelhosimao@gmail.com>
2026-08-03 20:05:39 -0300

    exerc01

---
+++ 03 - Estrutura Repetição/Conteudo/02 - Repeticao-whille.py
@@ -0,0 +1,49 @@
+# WHILE --------------- Enquanto
+# Contador = 1
+# While contador <= 8:     condicoes=True
+#    print('contador')
+#    contador = contador + 1
+#    contador += 1
+
+contador = 1
+
+print('=== incremento ===')
+while contador <= 8:
+    print(contador)
+    contador += 1 # incremento (adicionando +1)
+
+# decremento
+print('=== decremento ===')
+contador2 = 9
+
+while contador2 >= 1:
+    print(contador2)
+    contador2 -= 1
+
+# Interrupcao,
+print('=== Interrupcao ===')
+i = 1
+while i <= 12:
+    print(i)
+    if i == 10:
+        break
+    i += 1
+
+# Continue
+print('=== Continue ===')
+
+e = 1
+while e < 10:
+    e += 1
+    if e == 4:
+        continue
+    print(e)
+
+# else
+print('=== Else ===')
+r = 1
+while r < 6:
+    print(r)
+    r += 1
+else:
+    print('Condição deu Falos')
