1b95c00e07f5d3178f63ecc7cc24f33af54a4bf2
Fabiosimaocoelho <coelhosimao@gmail.com>
2026-08-11 21:33:08 -0300

    array

---
+++ 04 - Lista/conteudo/01 - array.py
@@ -0,0 +1,30 @@
+# Exemplo sem lista
+# nome1 = 'Fabio'
+# nome2 = 'Jessica'
+# nome3 = 'Francisco'
+# nome4 = 'Irani'
+
+# Exemplo com lista
+# nomes = ['Jessica' , 'Fabio' , 'Franciso','Irani']
+
+# Acessando valor na lista (Array)
+# frutas = ['Banana', 'Uva','Maça','Manga']
+# print(frutas[2])
+# print(frutas[1])
+# print(frutas[2]) # todos os valores de uma vez
+# tamanho_array_frutas = len(frutas)
+# print(f'No aray de frutas tem {tamanho_array_frutas} frutas')
+
+# Paseando pelo array usando for
+# carros = ['Gol' , 'Fusca', 'Brasilia','Opal']
+
+# for i in carros:
+#     print(f'Carr: {i}')
+
+carrinho_compras = [150, 260, 100, 50, 60]
+soma = 0
+
+for i in carrinho_compras:
+    soma = soma + i
+
+print(f'A soma total dos produtos foi de R$ {soma},00')
---
+++ 04 - Lista/exercicios/exe01.py
@@ -0,0 +1,34 @@
+# # Crie um programa para escrever a seguinte letra da musica 
+# # 1 elefante incomoda muita gente 
+# # 2 elefantes incomodam, incomodam muito mais
+# for i in range(1,11):
+#     if i == 1:
+#         print(f'{i} elefante incomoda muita gente ')
+#     else:
+#         print(f'{i} elefantes incomoda muito mais')
+
+# Calcule a media das notas e mostr no print.
+
+# notas = [5.5 ,8 ,9.2 ,5]
+# nota1 = notas[0]
+# nota2 = notas[1]
+# nota3 = notas[2]
+# nota4 = notas[3]
+
+# quantidade_total_notas = len(notas)
+# media = (nota1 + nota2 + nota3 + nota4) / quantidade_total_notas
+
+
+
+# # valor_array_medias = len(medias / 4)
+# print(f'a média é de {round(média,2')
+
+notas = [5.5 ,8 ,9.2 ,5]
+media = 0
+
+for i in notas:
+    media = media + i
+
+print(f'A media do aluna é {media}')
+
+
