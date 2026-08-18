08db16e18145868abeade8f3f18efe5053e7113f
Fabiosimaocoelho <coelhosimao@gmail.com>
2026-08-17 21:28:30 -0300

    exec05

--- 05 - Funções/Exercicio/exer03.py
+++ 05 - Funções/Exercicio/exer03.py
@@ -1,14 +1,13 @@
-# numeros = []
+numeros = []
 
-# valor_numeros = int(input('Favor informa numero:'))
-# for i in range(valor_numeros):
-#     digite = input('Qual numero?:')
-#     numeros.append(numeros)
+valor_numeros = int(input('Favor informa numero:'))
+for i in range(valor_numeros):
+    digite = input('Qual numero?:')
+    numeros.append(numeros)
 
-# if 8 % 2 == 0:
-#     print('lista par')
+if 8 % 2 == 0:
+    print('lista par')
 
-# else:
-#     print('impar')
+else:
+    print('impar')
 
-def impar_
